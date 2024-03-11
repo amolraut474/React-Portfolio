1a532f40e0ec2287b7b8f5ced8f0526788cc9716
raut amol <84304332+amolraut474@users.noreply.github.com>
2024-03-11 19:33:34 +0530

    Update Contact.js

--- src/Pages/Contact.js
+++ src/Pages/Contact.js
@@ -1,9 +1,14 @@
 import React from 'react'
-
+import Contactform from '../Compontas/Contact_Com/Contactform';
+import Card from '../Compontas/Contact_Com/Card';
+import Banner from '../Compontas/Banner-section/Banner';
 export default function Contact() {
   return (
     <div>
-      <h2>Contact</h2>
+      <Banner Contact_title_one="MY" Contact_title_two=" CONTACTS" Contact_para="Suspendisse viverra pretium neque ut tempus. Aenean et nulla non felis elementum." />
+      <Card/>
+      <Contactform/>
+
     </div>
   )
 }
