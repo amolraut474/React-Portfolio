f80de182a443d0d675781b48da582c751a721f4d
raut amol <84304332+amolraut474@users.noreply.github.com>
2024-03-11 19:32:41 +0530

    Update About.js

--- src/Pages/About.js
+++ src/Pages/About.js
@@ -1,9 +1,14 @@
 import React from 'react'
-
-export default function About() {
+import Contactform from '../Compontas/Contact_Com/Contactform';
+import Card from '../Compontas/Contact_Com/Card';
+import Banner from '../Compontas/Banner-section/Banner';
+export default function Contact() {
   return (
     <div>
-      <h2>About</h2>
+      <Banner Contact_title_one="MY" Contact_title_two=" CONTACTS" Contact_para="Suspendisse viverra pretium neque ut tempus. Aenean et nulla non felis elementum." />
+      <Card/>
+      <Contactform/>
+
     </div>
   )
 }
